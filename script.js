ffaf978f358861a0551fb25d8987dd219a52154a
ritabobita <67934328+ritabobita@users.noreply.github.com>
2023-10-11 14:43:10 -0400

    Finish Homepage URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let button = document.getElementById('click-here');
 let answer = document.getElementById('answer');
 
-let responseArray = ['Yes, absolutely!', 'Send an inquiry', 'Nah (jk please do!)'];
+let responseArray = ['Yes, absolutely!', 'Send an inquiry', 'Maybe tomorrow, maybe today', 'Signs point to yes'];
 
 function magic8() {
     let response = responseArray[Math.floor(Math.random() * responseArray.length)];
