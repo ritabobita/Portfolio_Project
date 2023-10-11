1761292ac0fa7a413c6e3d6fc8e8bca4d8236f58
ritabobita <67934328+ritabobita@users.noreply.github.com>
2023-10-10 20:02:47 -0400

    Add javascript and begin contact links and format js portion

--- script.js
+++ script.js
@@ -3,15 +3,9 @@ let answer = document.getElementById('answer');
 
 let responseArray = ['Yes, absolutely!', 'Send an inquiry', 'Nah (jk please do!)'];
 
-/*function magic8() {
+function magic8() {
     let response = responseArray[Math.floor(Math.random() * responseArray.length)];
-    return document.answer.innerHTML = response;
-}*/
+    return answer.innerHTML = response;
+}
 
-function showItem(){
-    answer.style.display = 'block';
-  }
-
-button.addEventListener('click', showItem);
-
-/*left off here, want to/need to figure out how to make magic8 function work*/
+button.addEventListener('click', magic8);
